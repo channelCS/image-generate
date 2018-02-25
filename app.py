ad11e2c0d18bbea84e3337faa07c3b25757248e6
Aditya Arora <adityadvlp@gmail.com>
2018-02-25 16:27:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,10 +65,10 @@ def predict():
 
 if __name__ == "__main__":
 	#decide what port to run the app in
-	#port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5000))
 
 	#run the app locally on the givn port
-	app.run(host='0.0.0.0', port=1245)
+	app.run(host='0.0.0.0', port=port)
 
 
 
